0d9d52ea7f5335db2000e6677a4f8587f8df587c
antonioatkinson <antonioatkinson123@gmail.com>
2017-07-08 15:47:20 -0400

    Added new text fields and additional functionality

--- ViewController.swift
+++ ViewController.swift
@@ -11,14 +11,22 @@ import UIKit
 class ViewController: UIViewController,UITextFieldDelegate {
     //Variables
     @IBOutlet weak var wageTitle: UILabel!
-    
     @IBOutlet weak var hoursWorked: UITextField!
     @IBOutlet weak var hourWage: UITextField!
     
     @IBOutlet weak var grossPay: UILabel!
+    @IBOutlet weak var netPay: UILabel!
+    @IBOutlet weak var federalTaxPercentage: UITextField!
+    @IBOutlet weak var stateTaxPercentage: UITextField!
+    @IBOutlet weak var federalTaxRate: UITextField!
+    @IBOutlet weak var stateTaxRate: UITextField!
+    @IBOutlet weak var ssnTaxPercentage: UITextField!
+    @IBOutlet weak var medicareTaxPercentage: UITextField!
+    @IBOutlet weak var ssnTaxRate: UITextField!
+    @IBOutlet weak var medicareTaxRate: UITextField!
+    
     
     //Functions
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,14 +39,50 @@ class ViewController: UIViewController,UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func calculatePay(_ sender: UIButton) {
+        let grossPay = CalcGrossPay()
+        CalcNetPay(grossPay: grossPay)
+    }
+    @IBAction func CalculateFederalTax(_ sender: Any) {
+        let grossPay = CalcGrossPay()
+        var federalTax:Double = Double(federalTaxRate.text!)!
+        federalTax = (1-((Double(grossPay)-federalTax)/Double(grossPay)))
+        federalTaxPercentage.text = String(federalTax)
+    }
+    @IBAction func CalculateStateTax(_ sender: Any) {
+        let grossPay = CalcGrossPay()
+        var stateTax:Double = Double(stateTaxRate.text!)!
+        stateTax = (1-((Double(grossPay)-stateTax)/Double(grossPay)))
+        stateTaxPercentage.text = String(stateTax)
+    }
+   
+    @IBAction func CalculateSSTax(_ sender: Any) {
+        let grossPay = CalcGrossPay()
+        var ssTax:Double = Double(ssnTaxRate.text!)!
+        ssTax = (1-((Double(grossPay)-ssTax)/Double(grossPay)))
+        ssnTaxPercentage.text = String(ssTax)
+    }
+    
+    @IBAction func CalculateMedicareTax(_ sender: Any) {
+        let grossPay = CalcGrossPay()
+        var medicareTax:Double = Double(medicareTaxRate.text!)!
+        medicareTax = (1-((Double(grossPay)-medicareTax)/Double(grossPay)))
+        medicareTaxPercentage.text = String(medicareTax)
+    }
+    func CalcGrossPay()->Int{
         let wageIn:Int = Int(hourWage.text!)!
         let hoursWorkedIn:Int = Int(hoursWorked.text!)!
         let grossPayOut = hoursWorkedIn*wageIn
         
         
         grossPay.text = "Gross Pay: $" + String(grossPayOut)
+        return grossPayOut;
     }
-
-
+    func CalcNetPay(grossPay:Int){
+        let fedTax:Double = Double(federalTaxRate.text!)!
+        let stTax:Double=Double(stateTaxRate.text!)!
+        let ssTax:Double = Double(ssnTaxPercentage.text!)!
+        let medicareTax:Double = Double(medicareTaxRate.text!)!
+        netPay.text = "Net Pay: $"+String(Double(grossPay)-fedTax-stTax-ssTax-medicareTax)
+    }
 }
 
